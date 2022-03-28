b140518e263ad899e5a50e4daf19ff3c98fa49f9
Felipe <dev.felipereis@gmail.com>
2022-03-28 20:37:54 -0300

    Home finalized

--- assets/main.js
+++ assets/main.js
@@ -1,53 +1,44 @@
-class SlideStories{
-    constructor(id){
-        this.slide = document.querySelector(`[data-slide="${id}"]`);
-        this.active = 0;
-        this.init();
-    }
-    activeSlide(index){
-        this.active = index;
-        
-        this.itens.forEach(item => item.classList.remove('active'));
-        this.itens[index].classList.add('active');
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
 
-        this.addThumbItens.forEach(item => item.classList.remove('active'));
-        this.addThumbItens[index].classList.add('active');
-    }
-    prev(){
-        if(this.active > 0){
-        this.activeSlide(this.active - 1);
-        }else{
-            this.activeSlide(this.active.length -  1);
-        }
-    };
-    next(){
-        if(this.active < this.itens.length - 1){
-        this.activeSlide(this.active + 1);
-        }else{
-            this.activeSlide(0);
-        }
-    }
-    addNavigation(){
-        const nextBtn = this.slide.querySelector('.slide-next');
-        const prevBtn = this.slide.querySelector('.slide-prev');
-        nextBtn.addEventListener('click', this.next);
-        prevBtn.addEventListener('click', this.prev);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    addThumbItens(){
-        this.itens.forEach(() => (this.thumb.innerHTML += `<span></span>`));
-        this.addThumbItens = Array.from(this.thumb.children);
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {
+            console.log(index);
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        }); 
+    }
+
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
     init(){
-        this.next = this.next.bind(this);
-        this.prev = this.next.bind(this);
-        this.itens = this.slide.querySelectorAll('.slide-itens > *');
-        this.thumb = this.slide.querySelector('.slide-thumb');
-        this.addThumbItens();
-        this.activeSlide(0);
-        this.addNavigation();
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
     }
 }
 
-new SlideStories('slide')
+const mobileNavbar = new MobileNavBar(
+    ".mobile-menu",
+    ".menu-nav",
+    ".menu-nav li",
+);
+
+mobileNavbar.init();
